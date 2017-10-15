1f700178f6161dff3bc4daf32557801101e4037e
Samba012 <32813104+Samba012@users.noreply.github.com>
2017-10-15 15:03:06 +0200

    test

--- app/src/main/java/com/example/samba/denavigation/MainActivity.java
+++ app/src/main/java/com/example/samba/denavigation/MainActivity.java
@@ -49,8 +49,4 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-    private void TestUpload() {
-        int i = 1 + 3;
-    }
-
 }
